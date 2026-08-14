a45563d90c95541c288485fc364c4b9747681679
Ludvik . <ludvik@quix.io>
2026-08-14 14:38:24 +0000

    Update file QuixLabNotebooks/quixlab/main.py

--- QuixLabNotebooks/quixlab/main.py
+++ QuixLabNotebooks/quixlab/main.py
@@ -3,13 +3,6 @@ import quixlab as ql
 canvas = ql.Canvas(title="My Notebook")
 
 
-@canvas.dataset(position=(91, 132), size=(1171, 729), code_height=147, viz={'datasetMode': 'ai'})
-def can_signals():
-    return ql.sql("""SELECT *
-    FROM can_signals_v13
-    LIMIT 100""")
-
-
 @canvas.datastore(position=(1562, 258), size=(560, 420), code_height=200)
 def datastore_1():
     return ql.datastore("datastore_1")
