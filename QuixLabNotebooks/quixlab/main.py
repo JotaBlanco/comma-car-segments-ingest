e72fda01ee2bedd32d54e113a8e97d825f817f8a
Ludvik . <ludvik@quix.io>
2026-08-17 09:01:38 +0000

    Update file QuixLabNotebooks/quixlab/main.py

--- QuixLabNotebooks/quixlab/main.py
+++ QuixLabNotebooks/quixlab/main.py
@@ -8,7 +8,7 @@ def datastore_1():
     return ql.datastore("datastore_1")
 
 
-@canvas.dataset(position=(791, 296), size=(560, 420), code_height=0, viz={'type': 'line', 'x': '', 'y': ''})
+@canvas.dataset(position=(791, 296), size=(752, 647), code_height=0, viz={'type': 'line', 'x': '', 'y': ''})
 def can_signals_v13():
     return ql.sql("""SELECT *
     FROM can_signals_v13
