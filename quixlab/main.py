45b4735b69ba14f491bf8a52339252543b50bc96
Ludvik . <ludvik@quix.io>
2026-09-24 09:29:58 +0000

    Update file quixlab/main.py

--- quixlab/main.py
+++ quixlab/main.py
@@ -1,6 +1,6 @@
 
 
-@canvas.ai(position=(719, 838), size=(650, 503), code_height=200, viz={'aiMode': 'agent'})
+@canvas.ai(position=(719, 838), size=(650, 503), code_height=200, viz={'aiAutoRun': True, 'aiMode': 'agent'})
 def ai_1():
-    """find Test definition 1 and creat"""
+    """find Test definition 1 and create test script ehre for it to validate on data from lakehouse"""
     # ql-ai-mode: agent
